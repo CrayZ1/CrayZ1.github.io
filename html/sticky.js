7271b9876bb5d0676f4fd9bf8832ec008a058618
CrayZ1 <53210510+CrayZ1@users.noreply.github.com>
2023-05-05 02:09:32 +1200

    First Website Design
    First Website Design

--- html/sticky.js
+++ html/sticky.js
@@ -2,16 +2,22 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("pagehead");
+var header = document.getElementById("navbarcenter");
+var topimage = document.getElementById("imagehead");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = topimage.offsetHeight - header.offsetHeight;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+  var scrollTop = window.pageYOffset;
+  var parts = [23, 23, 33, scrollTop / 400]
+  header.style.backgroundColor = `rgba(${ parts.join(',') })`;
 }
